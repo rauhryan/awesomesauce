1c96deeb0d0810bebcbf30a1c3623953d86754dc
Dru Sellers <dru@drusellers.com>
2012-06-03 14:14:09 -0500

    adding passport ideas

---
+++ src/Passport/PassportExtensionRegistry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FubuMVC.Core;
+using FubuMVC.Core.Registration.Nodes;
+
+namespace Passport
+{
+    public static class PassportConfiguration
+    {
+        public static Func<BehaviorChain, bool> RestrictedAction { get; set; }
+    }
+
+    public class PassportExtensionRegistry : IFubuRegistryExtension
+    {
+        public void Configure(FubuRegistry registry)
+        {
+            registry.Configure(graph =>
+            {
+                foreach(var c in graph.Behaviors)
+                {
+                    if(PassportConfiguration.RestrictedAction(c))
+                    {
+                        
+                    }
+                }
+            });
+        }
+    }
+
+    public class RestrictedPolicy
+    {
+        
+    }
+}
