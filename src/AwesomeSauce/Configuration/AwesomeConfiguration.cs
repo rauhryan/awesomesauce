6d2037bf5db101e44867918385665e18c9d14182
Dru Sellers <dru@drusellers.com>
2012-06-03 13:43:49 -0500

    html conventions

--- src/AwesomeSauce/Configuration/AwesomeConfiguration.cs
+++ src/AwesomeSauce/Configuration/AwesomeConfiguration.cs
@@ -1,5 +1,6 @@
 using System;
 using FubuCore;
+using FubuMVC.Core.UI.Configuration;
 
 namespace AwesomeSauce.Configuration
 {
@@ -14,5 +15,10 @@ namespace AwesomeSauce.Configuration
         public static Func<Type, bool> AwesomeEntities { get; set; }
 
         public static string TagProfile { get; set; }
+
+        public static bool IdField(AccessorDef arg)
+        {
+            return arg.Accessor.Name.Equals("Id");
+        }
     }
 } 
--- src/AwesomeSauce/Configuration/Html/AwesomeHtmlConventions.cs
+++ src/AwesomeSauce/Configuration/Html/AwesomeHtmlConventions.cs
@@ -11,14 +11,32 @@ namespace AwesomeSauce.Configuration.Html
             Profile(AwesomeConfiguration.TagProfile, tags =>
             {
                 tags.UseLabelAndFieldLayout<AwesomeFieldLayout>();
-                tags.Editors.If(a => AwesomeConfiguration.AwesomeEntities(a.ModelType))
+                tags.Editors.If(a => AwesomeConfiguration.AwesomeEntities(a.Accessor.PropertyType))
                     .BuildBy(BuildTypeAheadEntityFinder);
+
+                //generic way to wrap up an ID form line
+                tags.Displays.If(AwesomeConfiguration.IdField)
+                    .BuildBy(hiddenLabel);
+                tags.Labels.If(AwesomeConfiguration.IdField)
+                    .BuildBy(hiddenLabel);
+                tags.Editors.If(AwesomeConfiguration.IdField)
+                    .Modify(tag=>tag.Attr("type","hidden"));
+                //id line
+
+
             });
+        }
+
+        HtmlTag hiddenLabel(ElementRequest request)
+        {
+
+            return HtmlTag.Empty();
         }
 
         HtmlTag BuildTypeAheadEntityFinder(ElementRequest request)
         {
             return new HtmlTag("input")
+                .Attr("type","text")
                 .Attr("value", "type ahead");
         }
     }
