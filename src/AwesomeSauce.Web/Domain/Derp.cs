b1cab0e5eeba115e9d89efa8a049e19c13bfe3f9
Ryan Rauh <rauh.ryan@gmail.com>
2012-06-03 13:28:32 -0500

    view overrides are working

---
+++ src/AwesomeSauce.Web/Domain/Derp.cs
@@ -0,0 +1,11 @@
+﻿using AwesomeSauce.Domain;
+using MongoDB.Bson;
+
+namespace AwesomeSauce.Web.Domain
+{
+    public class Derp : AwesomeEntity
+    {
+        public ObjectId Id { get; set; }
+        public string Herp { get; set; }
+    }
+}
