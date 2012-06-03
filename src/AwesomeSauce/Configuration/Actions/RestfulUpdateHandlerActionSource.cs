353ae2decab419b1f291d920773dbe794b459c7e
Dru Sellers <dru@drusellers.com>
2012-06-03 09:36:31 -0500

    initial update support

---
+++ src/AwesomeSauce/Configuration/Actions/RestfulUpdateHandlerActionSource.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AwesomeSauce.Domain;
+using AwesomeSauce.Handlers;
+using FubuCore;
+using FubuCore.Reflection;
+using FubuMVC.Core.Registration;
+using FubuMVC.Core.Registration.Nodes;
+
+namespace AwesomeSauce.Configuration.Actions
+{
+    public class RestfulUpdateHandlerActionSource
+    {
+        private static readonly string _methodName =
+            ReflectionHelper.GetMethod<RestfulPatchHandler<string>>(x => x.Patch(null)).Name;
+
+        public IEnumerable<ActionCall> FindActions(TypePool types)
+        {
+            return from entities in types.TypesMatching(t => t.CanBeCastTo<AwesomeEntity>())
+                   where entities.IsConcrete()
+                   select typeof(RestfulPatchHandler<>).MakeGenericType(entities)
+                    into handlerType
+                    let method = handlerType.GetMethod(_methodName)
+                    select new ActionCall(handlerType, method);
+        }
+
+         
+    }
+}
---
+++ src/AwesomeSauce/Handlers/RestfulPatchHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace AwesomeSauce.Handlers
+{
+    public class RestfulPatchHandler<TEntity> where TEntity : class
+    {
+        public object Patch(object o)
+        {
+            return null;
+        }
+    }
+}
